d008b4be36b6096ba80155607fb30cd8f053c030
Vp <59320967+HerraVp@users.noreply.github.com>
2021-02-07 14:12:58 +0200

    Add files via upload

---
+++ DiscordSpammer/main.py
@@ -0,0 +1,21 @@
+import pyautogui
+from time import sleep
+
+time = 0
+while time != 10:
+    time += 1
+    print("DcSpammer waiting... " + str(time))
+
+
+def spam(msg, maxMsg):
+    count = 0
+    while count != maxMsg:
+        count += 1
+        print("Send message: " + str(count))
+        pyautogui.write(msg)
+        pyautogui.press("enter")
+        if count == 5 or count == 5 * 2 or count == 5 * 3:
+            sleep(8)
+
+
+spam("Rotta", 20)
